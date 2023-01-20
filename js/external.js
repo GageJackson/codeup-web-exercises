20744827dadd5adf59f60ced55b9bf5c5d808da5
Gage Jackson <wm.gagejackson@gmail.com>
2023-01-20 16:38:21 -0600

    Test

--- js/external.js
+++ js/external.js
@@ -1,6 +1,46 @@
 "use strict";
-console.log("hello, externally");
-alert("This is an alert");
-confirm("Confirm that you can see this");
-var isPrompt = prompt("Write stuff here");
-alert("You wrote: " + isPrompt);
+
+//Intro prompts and alerts
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+var usersFavoriteColor = prompt("What is your favorite color?");
+alert("Great, " + usersFavoriteColor + "is my favorite color too!")
+
+
+var daysRentingLittleMermaid = prompt("How many days are you renting the 'Little Mermaid'?")
+var daysRentingBrotherBear = prompt("How many days are you renting the 'Brother Bear'?")
+var daysRentingHercules = prompt("How many days are you renting the 'Hercules'?")
+var dayRentalRate = 3;
+var totalRentalCost = (daysRentingLittleMermaid + daysRentingBrotherBear + daysRentingHercules) * dayRentalRate;
+alert("That will cost you: $" + totalRentalCost);
+
+var googleHourlyPayRate = 400;
+var facebookHourlyPayRate = 350;
+var amazonHourlyPayRate = 380;
+
+var googleHoursWorked = prompt("How many hours did you work at Google this week?");
+var facebookHoursWorked = prompt("How many hours did you work at Facebook this week?");
+var amazonHoursWorked = prompt("How many hours did you work at Amazon this week?");
+
+var weeklyPay = (googleHourlyPayRate * googleHoursWorked) + (facebookHourlyPayRate * facebookHoursWorked) + (amazonHourlyPayRate * amazonHoursWorked);
+alert("Great! You earned $" + weeklyPay + " this week!");
+
+var classSize = Math.floor((Math.random() * 100) + 1);
+var maxClassSize = 50;
+var classIsFull = false;
+
+if(classSize >= maxClassSize){
+    classIsFull = true;
+}
+console.log(classSize);
+var isScheduleConflict = confirm("Does this class conflict with your schedule?");
+if(!classIsFull && !isScheduleConflict){
+    alert("You are enrolled in the class!")
+}
+if(classIsFull){
+    alert("There are too many students in this class. You are not enrolled.");
+}
+if(isScheduleConflict){
+    alert("Maybe try again next semester!")
+}
+
