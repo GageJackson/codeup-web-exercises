1b2b6900dc7048ca5095802468600d1dd3a376ce
Gage Jackson <wm.gagejackson@gmail.com>
2023-01-23 09:36:03 -0600

    Test

--- js/external.js
+++ js/external.js
@@ -6,14 +6,16 @@ alert("Welcome to my Website!");
 var usersFavoriteColor = prompt("What is your favorite color?");
 alert("Great, " + usersFavoriteColor + " is my favorite color too!");
 
-
+//Cost calculator for renting 3 predetermined movies at $3 / day each.
 var daysRentingLittleMermaid = parseInt(prompt("How many days are you renting the 'Little Mermaid'?"));
 var daysRentingBrotherBear = parseInt(prompt("How many days are you renting the 'Brother Bear'?"));
 var daysRentingHercules = parseInt(prompt("How many days are you renting the 'Hercules'?"));
 var dayRentalRate = 3;
+
 var totalRentalCost = (daysRentingLittleMermaid + daysRentingBrotherBear + daysRentingHercules) * dayRentalRate;
 alert("That will cost you: $" + totalRentalCost);
 
+//Weekly pay based on amount of hours worked per week
 var googleHourlyPayRate = 400;
 var facebookHourlyPayRate = 350;
 var amazonHourlyPayRate = 380;
@@ -25,6 +27,7 @@ var amazonHoursWorked = prompt("How many hours did you work at Amazon this week?
 var weeklyPay = (googleHourlyPayRate * googleHoursWorked) + (facebookHourlyPayRate * facebookHoursWorked) + (amazonHourlyPayRate * amazonHoursWorked);
 alert("Great! You earned $" + weeklyPay + " this week!");
 
+//Checks to see if user can take class based on randomly generated class size and a prompt if user can fit class in schedule
 var classSize = Math.floor((Math.random() * 100) + 1);
 var maxClassSize = 50;
 var classIsFull = false;
@@ -43,11 +46,13 @@ if(classIsFull){
 if(isScheduleConflict) {
     alert("Maybe try again next semester!")
 }
+
+//Asks user if they are a premium member and how many items they purchased in order to use a coupon
 var isCouponValid = true;
 var isPremiumMember = confirm("Are you a premium member?");
 var itemsPurchased = parseInt(prompt("How many items are you purchasing today?"));
 
-if(isCouponValid && (isPremiumMember||itemsPurchased>=2)){
+if(isCouponValid && (isPremiumMember || itemsPurchased >=2)){
     alert("You can use the coupon!")
 } else {
     alert("You cannot use the coupon...")
